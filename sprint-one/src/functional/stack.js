b9fbe480a75aded75a41a3004057b774ba494e53
bpdavitt <bpdavitt@gmail.com>
2019-05-29 14:03:12 -0500

    Created the methods using functional instantiation. Passed SpecRunner tests.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,3 +1,9 @@
+// input: function invocation on a value
+// output: return an object containing methods to use on the stack push, pop, size
+// constraints: must use object and functional instantiation
+// strategy: 
+// transformation steps:
+// psuedo:
 var Stack = function() {
   var someInstance = {};
 
@@ -6,12 +12,18 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var popped = storage[someInstance.size()-1];
+    delete storage[someInstance.size()-1];
+    return popped;
   };
 
   someInstance.size = function() {
+    var storageKeys = Object.keys(storage);
+    return storageKeys.length;
   };
 
   return someInstance;
