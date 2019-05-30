f86cfdf45f9f7e47ef0687e0350fe285c20224d0
bpdavitt <bpdavitt@gmail.com>
2019-05-30 11:21:32 -0500

    Linked list implemented

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var nodeObj = Node(value);
+    if (list.head === null) {
+      list.tail = nodeObj;
+      list.head = nodeObj;
+      return undefined;
+    }
+    list.tail.next = nodeObj;
+    list.tail = nodeObj;
   };
 
   list.removeHead = function() {
+    let oldHead = list.head.value;
+    list.head = list.head.next;
+    return oldHead;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    
+    let currentNode = node || list.head;
+    if(currentNode.value === target) {
+      return true;
+    } else {
+      if(currentNode.next === null){
+        return false;
+      }
+      return list.contains(target, currentNode.next);
+    }
   };
-
+  
   return list;
 };
 
