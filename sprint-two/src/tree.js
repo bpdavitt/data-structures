5c3e6cfbbe002c3765e12924447b9c19f4ebc23b
bpdavitt <bpdavitt@gmail.com>
2019-05-30 15:12:54 -0500

    Completed implementation of tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -12,10 +12,19 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  this.children.push(Tree(value))
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for(let i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
