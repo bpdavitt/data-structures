6b9e4f70fa443ca1b677e7a0b2268b563c3b2919
bpdavitt <bpdavitt@gmail.com>
2019-05-31 10:59:37 -0500

    Implemented binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,50 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
+
+
+BinarySearchTree.prototype.insert = function(v){
+  if (v < this.value) {
+    if (this.left === null) {
+      this.left = new BinarySearchTree(v);
+    } else {
+      this.left.insert(v);
+    }
+  }
+  if (v > this.value) {
+    if (this.right === null) {
+      this.right = new BinarySearchTree(v);
+    } else {
+      this.right.insert(v);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function(v) {
+  if (this.value === v) {
+    return true;
+  }
+  if (v < this.value && this.left !== null) {
+    return this.left.contains(v);
+  }
+  if (v > this.value && this.right !== null) {
+    return this.right.contains(v);
+  }
+  return false;
+}
+
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+  callback(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+}
 
 
 /*
