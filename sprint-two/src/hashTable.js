0c2dcdc140fc28289d93587e7d6920f47498babd
bpdavitt <bpdavitt@gmail.com>
2019-05-31 19:56:26 -0500

    Started work on resizing hashmap

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,9 +3,14 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  this._count++;
+  if (this._count/this._limit >= 0.75){
+    this._storage.resize('expand');
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index)) {
     let bucket = this._storage.get(index);
@@ -32,6 +37,10 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  this._count--;
+  if (this._count/this._limit < 0.25){
+    this._storage.resize('contract');
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
   let bucket = this._storage.get(index);
   for (let i = 0; i < bucket.length; i++) {
@@ -43,6 +52,25 @@ HashTable.prototype.remove = function(k) {
 };
 
 
+HashTable.prototype.resize = function(action) {
+  let oldLength = this._limit;
+  if (action === 'expand') {
+    this._limit *= 2;
+  }
+  if (action === 'contract'){
+    this._limit /= 2;
+  
+  }
+  for (let i = 0; i < oldLength; i++){
+    let bucket = this._storage.get(i);
+    for(let j = 0; j < bucket.length; j++){
+      //need to remove each key/value and rehash into new hashmap
+      let item = bucket[j];
+      bucket.splice(j,1);
+      this._storage.insert(item[0], item[1]);
+    }
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
